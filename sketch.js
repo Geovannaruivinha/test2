8a4c3d6f3aed58ef018dd394820553ced6c2cd47
Geovannaruivinha <98528323+Geovannaruivinha@users.noreply.github.com>
2022-01-27 09:22:13 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var bola;
+function setup() {
+  createCanvas(400,400);
+  bola=createSprite(200,200,2,2);
+  bola.shapeColor='pink';
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites();
+
+}
+
+
+
+
